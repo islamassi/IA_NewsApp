98c8dd56d6c536ea22580d2c05654c69a8a48088
islamassi <islam_assi@hotmail.com>
2020-05-03 00:41:37 +0300

    Removing API key

--- app/src/main/java/com/islamassi/latestnews/Constants.kt
+++ app/src/main/java/com/islamassi/latestnews/Constants.kt
@@ -2,5 +2,6 @@ package com.islamassi.latestnews
 
 object Constants {
     const val BASE_URL = "https://newsapi.org/"
-    const val API_KEY = "7019973f03494525b62199f2e92fe71f"
+    //TODO("add NewsApi key here")
+    const val API_KEY = "ADD_YOUR_API_KEY_HERE"
 }
